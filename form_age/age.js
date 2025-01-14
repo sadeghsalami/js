6b92d6843a901ad0a688d4aef4c07608c7da2aad
sadeghsalami <m.sadeghsalami@yahoo.com>
2025-01-14 20:13:58 +0330

    25 day 1403

--- form_age/age.js
+++ form_age/age.js
@@ -3,7 +3,9 @@ let l_name = document.querySelector('.l_name')
 let age = document.querySelector('.age')
 let register = document.querySelector('.register')
 let frm = document.querySelector('.frm_age')
-
+let table1 = document.querySelector('.table1')
+let table2 = document.querySelector('.table2')
+let tableNum = document.querySelector('.tableNum')
 
 
 
@@ -17,10 +19,26 @@ function getNames(e){
     e.preventDefault();
     let inputAge = age.value;
     if (inputAge < 18){
-        console.log(inputAge);
+        let tableNum
+        let table1 = `
+            <tr>
+                <td class ='table1 tr_inputAge'>  ${tableNum} </td>
+            </tr>
+        `
+        tableNum.innerHTML += table1
+
+
     }
     else
     if(inputAge >= 18){
-        console.log(inputAge);
+        let tableNum
+        let table2 = `
+        <tr>
+            <td class ='table2 tr_inputAge'> ${tableNum} </td>
+        </tr>
+    `
+    tableNum.innerHTML += table2
+
+
     }
 }
