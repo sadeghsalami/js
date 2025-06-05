96ebb99d3e44cf2abbbd1425f40f6362c7e4b4ed
sadeghsalami <m.sadeghsalami@yahoo.com>
2025-06-05 11:37:09 +0330

    15 khordad 1404

--- comision/commision.js
+++ comision/commision.js
@@ -1,38 +1,20 @@
 let rahni  = document.querySelector(".rahn#rahn");
 let ejarei = document.querySelector(".ejare#ejare");
 
-let frm = document.querySelector(".frm_commision");
-
 let btn_rahn = document.querySelector(".cmp_rhn");
 let btn_ejare = document.querySelector(".cmp_ejr");
 
 let msg = document.querySelector(".msg")
 let per_rahn = document.querySelector(".rahniValue");
-let per_ejare = document.querySelector(".");
+
+
+let RESULT = document.querySelector(".result");
 
 eventListeners();
 function eventListeners(){
-    btn_rahn.addEventListener("submit", getRahn);
-    btn_ejare.addEventListener("submit", getEjare);
+    btn_rahn.addEventListener("click", getRahn);
 }
 
-function getRahn (e) {
-    e.preventDefault();
-    let rahniValue = rahni.value;
+function getRahn () {
     
-
-    if (rahniValue > 1 && rahniValue * 3 /100){
-        msg.textContent = "RAHN :";
-        <p> ${per_rahn} </p>
-        per_rahn.innerHTML += rahniValue;
-    }
-}
-function getEjare (e){
-    e.preventDefault();
-    let ejareiValue = ejarei.value;
-    
-    if(ejareiValue > 1 && ejareiValue * 100 / 3){
-        msg.textContent = "EJARE  :";
-
-    }
 }
